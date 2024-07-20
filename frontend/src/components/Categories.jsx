0cd4fbf5e7e0d7b7d7e1d7130dd99d09c90fa13d
dammar093 <dammarrana093@gmail.com>
2024-07-20 18:12:41 +0545

    add catogories page

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import CategoryCard from '../components/CategoryCard'
-import categories from '../data/categories'
-
+import categories from "../data/categories"
 const Categories = () => {
   return (
     <section className=' w-full'>
       <div className='flex items-center gap-2 scrollbar-hide overflow-x-scroll md:overflow-x-hidden md:grid grid-cols-8'>
         {
-          categories.map(item => (
-            <div key={item.id}>
-              <CategoryCard category={item} />
+          categories.map(category => (
+            <div key={category.id}>
+              <CategoryCard category={category} />
             </div>
           ))
         }
