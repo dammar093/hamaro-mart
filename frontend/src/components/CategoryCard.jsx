ae129ace0dbe986b8057d4c4b5f75ecd9aec9794
dammar093 <dammarrana093@gmail.com>
2024-07-20 17:45:12 +0545

    make category uppercase

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -4,7 +4,7 @@ const CategoryCard = ({ category }) => {
   return (
     <Link to={`/category/category=${category.title}`} className='w-[45vw] h-[170px] md:w-[150px] md:h-[150px] bg-white shadow-sm py-2 flex items-center justify-center flex-col rounded overflow-hidden'>
       <img className='w-[140px] h-[120px] md:w-[100px] md:h-[100px] object-cover hover:scale-110 transition-all' src={category.image} alt="" />
-      <h2 className='text-md text-gray-700 font-medium'>{category.title}</h2>
+      <h2 className='text-md text-gray-700 font-medium uppercase'>{category.title}</h2>
     </Link>
   )
 }
