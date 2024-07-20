b56b7c05fea010131852a14c82ab3a0ab9a6503b
dammar093 <dammarrana093@gmail.com>
2024-07-20 18:13:45 +0545

    add mb in categories page

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -4,7 +4,7 @@ import categories from '../data/categories'
 
 const Categoreis = () => {
   return (
-    <div className='mx-auto w-full my-4'>
+    <div className='mx-auto w-full my-4 mb-20'>
       <h2 className='text-xl uppercase text-gray-600 mtext-[14px] font-semibold md:text-xl md:font-medium py-2'>Categories</h2>
       <div className='grid grid-cols-2 gap-4 w-full  md:grid-cols-4 lg:grid-cols-6 md:gap-2 '>
         {
